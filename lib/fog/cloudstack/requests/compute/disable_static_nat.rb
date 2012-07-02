fd7a0d30968e6796ec53527d87245b3e2e130ae5
atistler <atistler@gmail.com>
2012-07-02 17:28:36 -0400

    disable static nat

--- lib/fog/cloudstack/requests/compute/disable_static_nat.rb
+++ lib/fog/cloudstack/requests/compute/disable_static_nat.rb
@@ -6,7 +6,7 @@ module Fog
         # Disables a user account.
         #
         # {CloudStack API Reference}[http://download.cloud.com/releases/2.2.0/api_2.2.14/user/disableStaticNat.html]
-        def enable_static_nat(options={})
+        def disable_static_nat(options={})
           options.merge!(
               'command' => 'disableStaticNat'
           )
